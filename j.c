c2c78d61d3405ac4022bab98e36b464947c12b94
logitha <loga848.456@gmail.com>
2016-07-18 18:42:55 +0530

    Create j.c

---
+++ j.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b=500,c=50,d=9,f=30,k,j,k1,k2,k3;
+clrscr();
+scanf("%d",&a);
+if(a<=1000)
+{
+k=b/100;
+k1=c/10;
+k2=d/3;
+k3=f/15;
+j=k+k1+k2+k3;
+printf("%d",j);
+}
+getch();
+}
+
+
+
